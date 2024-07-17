b6e4a30515dffd28ef74dc72d3a28ef82aef39a4
Soumili <soumilimukherjee20@gmail.com>
2024-07-17 20:06:33 +0530

    add frontend and backend

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed And Developed By Soumili
+    </footer>
+  )
+}
+
+export default Footer
